4fec03b20a25c3f425edf698305db73f944e9fbf
wilmerdanaiffer <wilmerdanaiffer@hotmail.com>
2024-09-25 20:30:34 -0500

    Add files via upload

---
+++ Helpers/PeliculaExisteAttribute.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace PeliculasApiRestFul.Helpers
+{
+    public class PeliculaExisteAttribute : Attribute, IAsyncResourceFilter
+    {
+        private readonly ApplicationDbContext applicationDbContext;
+
+        public PeliculaExisteAttribute(ApplicationDbContext applicationDbContext)
+        {
+            this.applicationDbContext = applicationDbContext;
+        }
+        public async Task OnResourceExecutionAsync(ResourceExecutingContext context, ResourceExecutionDelegate next)
+        {
+            var peliculaIdObject = context.HttpContext.Request.RouteValues["peliculaId"];
+            if (peliculaIdObject == null)
+            {
+                return;
+            }
+            var peliculaId = int.Parse(peliculaIdObject.ToString());
+            var existePelicula = await applicationDbContext.Peliculas.AnyAsync(x => x.Id == peliculaId);
+            if (!existePelicula)
+            {
+                context.Result = new NotFoundResult();
+            }
+            else
+            {
+                await next();
+            }
+
+        }
+    }
+}
---
+++ Helpers/TypeBinder.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Newtonsoft.Json;
+
+namespace PeliculasApiRestFul.Helpers
+{
+    public class TypeBinder<T> : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            var nombrePropiedad = bindingContext.ModelName;
+            var proveedorValores = bindingContext.ValueProvider.GetValue(nombrePropiedad);
+            if (proveedorValores == ValueProviderResult.None)
+            {
+                return Task.CompletedTask;
+            }
+            try
+            {
+                var valorDeserializado = JsonConvert.DeserializeObject<T>(proveedorValores.FirstValue);
+                bindingContext.Result = ModelBindingResult.Success(valorDeserializado);
+            }
+            catch
+            {
+                bindingContext.ModelState.TryAddModelError(nombrePropiedad, "Valor no válido para el tipo <T>");
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
